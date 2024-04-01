ccbd081eaffdec2e939b6341af9a0f9790839e5c
LinoDF <165563095+LinoDF@users.noreply.github.com>
2024-04-01 19:51:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
-
 # import libraries
 import minecraft_launcher_lib
 import os
+from os import system
 import subprocess
+# set window title
+system("title " + "CleanLauncher")
 # set launcher path
 winusr = os.environ["USERNAME"]
 minedir = "C://Users//" + winusr + "//AppData//Roaming//.cleanlauncher"
 verdir = "C://Users//" + winusr + "//AppData//Roaming//.cleanlauncher//versions"
 if os.path.exists(verdir) == False:
     os.makedirs(verdir)
-#set game version and username
+# list game versions
+print("Latest release: " + minecraft_launcher_lib.utils.get_latest_version()["release"])
+print()
+print("Latest snapshot: " + minecraft_launcher_lib.utils.get_latest_version()["snapshot"])
+print()
+print("Installed versions:")
+for element in minecraft_launcher_lib.utils.get_installed_versions(minedir):
+    print(element["id"])
+print()
+# set game version and username
 minever = input("Version: ")
-mineusr = input("Name: ")
+print()
+mineusr = input("Userame: ")
+print()
 settings = {
     "username": mineusr,
-    "uuid": "The UUID",
-    "token": "The acces token"
-}
+    "uuid": "",
+    "token": ""
+    }
 # generate version json
 minevers = minecraft_launcher_lib.utils.get_available_versions(minedir)
 jver = open(verdir + "//versions.json", "w")
 print(minevers, file = jver)
 jver.close()
 # install and run game
+print("Please wait...")
 minecraft_launcher_lib.install.install_minecraft_version(minever, minedir)
 runmine = minecraft_launcher_lib.command.get_minecraft_command(minever, minedir, settings)
 subprocess.run(runmine)
